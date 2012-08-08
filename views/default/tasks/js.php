95717bc5a4a2b5f9d7f3b01bd01cf7bb67029567
Sem <sembrestels@riseup.net>
2012-08-08 03:32:23 +0200

    Added getSelection feature.

--- views/default/tasks/js.php
+++ views/default/tasks/js.php
@@ -4,10 +4,27 @@
  */
 ?>
 
+elgg.provide('elgg.ui.getSelection');
+
+elgg.ui.getSelection = function () {
+	if (window.getSelection) {
+		return window.getSelection().toString();
+	}
+	else if (document.getSelection) {
+		return document.getSelection();
+	}
+	else if (document.selection) {
+		// this is specifically for IE
+		return document.selection.createRange().text;
+	}
+}
 
 $(function(){
 	$('.elgg-menu-extras .elgg-menu-item-task a').click(function(){
-		var title = encodeURIComponent($('h2.elgg-heading-main').text());
+		var title = encodeURIComponent(elgg.ui.getSelection());
+		if (!title) {
+			title = encodeURIComponent($('h2.elgg-heading-main').text());
+		}
 		$(this).attr('href', $(this).attr('href') + "&title=" + title);
 	});
 });
